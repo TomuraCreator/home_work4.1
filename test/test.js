465bfca30c1b563b7a89e7e00cbfcbd226065368
tery <vladislavutkin92@gmail.com>
2019-03-15 22:14:08 +0300

    обновлен тест

--- test/test.js
+++ test/test.js
@@ -1,8 +1,20 @@
 import {showColor} from '../src/js/app.js';
 
-test('should add two numbers', () => {
+test('critical HP', () => {
+  const characters = { name: 'маг', health: 14 };
+  const received = 'critical';
+  const expected = showColor(characters);
+  expect(received).toBe(expected);
+});
+test('wounded HP', () => {
   const characters = { name: 'маг', health: 40 };
   const received = 'wounded';
+  const expected = showColor(characters);
+  expect(received).toBe(expected);
+});
+test('healthy HP', () => {
+  const characters = { name: 'маг', health: 70 };
+  const received = 'healthy';
   const expected = showColor(characters);
   expect(received).toBe(expected);
 });
